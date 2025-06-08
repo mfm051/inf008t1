e250781556f7ad54cf4ecfaf4a9c02b0064b9395
Mateus Matos <98349389+mfm051@users.noreply.github.com>
2025-06-08 20:02:55 -0300

    Simplifica certificado de evento

--- events/EventCertificate.java
+++ events/EventCertificate.java
@@ -12,23 +12,9 @@ public class EventCertificate {
     }
 
     public String getMessage() {
-        if (event.isPresenter(attendee))
-            return getPresenterMessage();
-
-        if (event.isAttendee(attendee))
-            return getParticipationMessage();
-
-        return "Participante não registrado no evento";
-    }
-
-    private String getParticipationMessage() {
-        return "Certificamos a participação do(a) " + attendee.getFullInfo() + " no evento " + event.getTitle()
-                + " realizado em " + event.getLocation() + " em " + event.getReadableDate() +
-                " com uma carga horária de " + event.getReadableWorkload(attendee);
-    }
-
-    private String getPresenterMessage() {
-        return "Certificamos a participação do(a) " + attendee.getFullInfo() + " como apresentador no evento " + event.getTitle()
+        return "Certificamos a participação do(a) " + attendee.getFullInfo()
+                + (event.isPresenter(attendee) ? " como apresentador" : "") +
+                " no evento " + event.getTitle()
                 + " realizado em " + event.getLocation() + " em " + event.getReadableDate() +
                 " com uma carga horária de " + event.getReadableWorkload(attendee);
     }
